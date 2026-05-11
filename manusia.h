771962550205bf2b77ad2b124ffa47589625c65e
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-05-11 20:28:41 +0700

    menambahkan constructor dan descroctor pada class manusia

--- manusia.h
+++ manusia.h
@@ -10,7 +10,14 @@ public:
     string name;
     jantung varJantung;
 
-    
+    manusia(string pName)
+        : name(pName) {
+        cout << name << " hidup\n";
+    }
+    ~manusia() {
+        cout << name << " mati\n";
+    }
 };
 
 
+#endif
