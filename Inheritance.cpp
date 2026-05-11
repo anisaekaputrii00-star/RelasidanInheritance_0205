75819d92f4485e512c08c2f3b1e43500435d0f42
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-05-11 20:38:45 +0700

    menambahkan atribut dan constructor class orang

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -3,4 +3,15 @@
 using namespace std;
 
 class orang {
+public:
+    string nama;
+
+    orang(string pNama) :
+        nama(pNama) {
+        cout << "orang dibuat\n" << endl;
+    }
+
+    ~orang() {
+        cout << "orang dihapus\n" << endl;
+    }
 };
