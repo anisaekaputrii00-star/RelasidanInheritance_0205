fc57a55934eacedf1c35541b0ab749108660f148
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-05-11 21:39:50 +0700

    menambahkan atribut dan constructor class User

--- tugasoraktikum.cpp
+++ tugasoraktikum.cpp
@@ -3,5 +3,16 @@
 using namespace std;
 
 class user{
-    
+    public:
+    static int globalId;
+    int id;
+    string nama;
+    string email;
+
+    User(string nama, string email)
+    {
+        this->nama = nama;
+        this->email = email;
+        id = generateId();
+    }
 }
