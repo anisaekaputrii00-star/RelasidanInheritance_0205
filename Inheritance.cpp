6295a147aebae1cb1080d2cecb17168d1f4c44b8
anisaekaputrii00-star <anisaekaputrii00@gmail.com>
2026-05-11 20:44:00 +0700

    menambahkan atribut dan constructor class pelajar

--- Inheritance.cpp
+++ Inheritance.cpp
@@ -21,4 +21,14 @@ public:
 };
 
 class pelajar : public orang {
+    public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
+
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
 };
